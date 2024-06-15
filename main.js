bce29b79a50039d00fd49899d6847a8c56aa62c9
userdotnet10 <alainlobato100@gmail.com>
2024-06-14 19:41:50 -0600

    DB Connect

--- main.js
+++ main.js
@@ -20,3 +20,36 @@ var seconds = Math.floor((distance % _minute) / _second);
 document.getElementById('countdown').innerHTML = '<div class="box">'+days + '<div class="text">DIAS</div></div><div class="box">' + hours + '<div class="text">HORAS</div></div><div class="box">' + minutes + '<div class="text">MIN</div></div><div class="box">' + seconds+'<div class="text">SEG</div></div>';
 }        
 timer = setInterval(showRemaining, 1000);
+
+function submitForm() {
+   const form = document.forms['rsv'];
+   const data = new FormData(form);
+
+   if (!form.querySelector('input[name="Asistencia"]:checked')) {
+      alert('Por favor, selecciona una opción de asistencia.');
+      return false;
+   }
+
+   fetch('https://script.google.com/macros/s/AKfycbxqxZyLUpjinxpjbqHdDSJpdlA1g3Gdj1oarFx2EVsjDvxqBWlop1OQVMEeajFTVdspfw/exec', {
+     method: 'POST',
+     body: data
+   })
+   .then(response => response.json())
+   .then(result => {
+
+      cleanInput()
+      console.log('Success:', result);
+      alert('Formulario enviado correctamente');
+
+   })
+   .catch(error => {
+     console.error('Error:', error);
+     alert('Hubo un error al enviar el formulario');
+   });
+
+   return false;
+ }
+
+function cleanInput(){
+   document.getElementById('rsv').reset();
+}
